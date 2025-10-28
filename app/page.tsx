26bfb3aac58462b5ab407d12e60b02767192bb85
atharva <aatiw69@gmail.com>
2025-10-28 10:15:01 +0530

    trpc setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client"
+
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 const Page = () => {
+  const trpc = useTRPC();
+  const {data:users} = useQuery(trpc.getUsers.queryOptions());
   const something = true;
 
   return (
